e3f551feb08c5dfadae44ca46437889661bcd056
Huy2003zzz <huy072806@gmail.com>
2023-11-20 01:41:42 +0700

    update

---
+++ resources/views/layout_index/footer.blade.php
@@ -0,0 +1,54 @@
+ <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="address">
+                        <h4>Địa chỉ của chúng tôi</h4>
+                        <h6> BookStore, Cửa hàng thứ 4 Bên cạnh tòa nhà đó, Hoa Kỳ</h6>
+                        <h6>Gọi : 800 1234 5678</h6>
+                        <h6>Email : info@bookstore.com</h6>
+                    </div>
+                    <div class="timing">
+                        <h4>Thời gian</h4>
+                        <h6>Thứ Hai - Thứ Sáu: 7 giờ sáng - 10 giờ tối</h6>
+                        <h6>​​Thứ 7: 8 giờ sáng - 10 giờ tối</h6>
+                        <h6>​Chủ nhật: 8 giờ sáng - 11 giờ tối</h6>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="navigation">
+                        <h4>Dẫn Đường</h4>
+                        <ul>
+                            <li><a href="{{route('index')}}">Trang Chủ</a></li>
+                            <li><a href="{{ route('introduce') }}">Về chúng tôi</a></li>
+                            <li><a href="{{ route('all_book') }}">Các sản phẩm</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-5">
+                    <div class="form">
+                        <h3>Liên hệ nhanh với chúng tôi</h3>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.4850745445833!2d106.7019817142155!3d10.77411129232313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752f46eae30399%3A0x637b9183f6a8e0a7!2sFahasa!5e0!3m2!1svi!2s!4v1611296931856!5m2!1svi!2s" width="500" height="350" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copy-right">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h5>(C) 2017. All Rights Reserved. BookStore Wordpress Theme</h5>
+                    </div>
+                   <div class="row">
+                        <div class="col footer-social">
+                            <a href="#"><i class="fab fa-facebook-f"></i></a> 
+                            <a href="#"><i class="fab fa-twitter"></i></a> 
+                            <a href="#"><i class="fab fa-google-plus-g"></i></a> 
+                            <a href="#"><i class="fab fa-instagram"></i></a> 
+                            <a href="#"><i class="fab fa-pinterest"></i></a>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+    </footer>
---
+++ resources/views/layout_index/header.blade.php
@@ -0,0 +1,139 @@
+<header>
+    <a href="tel:0779 750 123">
+        <div class="hotline">
+            <span class="before-hotline">Hotline:</span>
+            <span class="hotline-number">0779 750 123</span>
+        </div>
+    </a>
+    <div class="header-top">
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <span class="ph-number"><i class="fa fa-truck"></i> {{ __('free ship') }} </span>
+                </div>
+                <div class="col-md-5 ">
+                    <span style="float: left;" class="ph-number"><i class="fa fa-phone"></i> {{ __('phone') }}: 0779 750 123 || 0945 555 666</span>
+                </div>
+                <div class="col-md-4">
+                    <div style="float: right; padding: 3%">
+                        <a href="{!! route('user.language', ['en']) !!}">
+                            <img src="{{ asset('images/icon/tienganh.png') }}" height="30px" width="30px">
+                        </a>
+                        <a href="{!! route('user.language', ['vi']) !!}">
+                            <img src="{{ asset('images/icon/tiengviet.png') }}" height="30px" width="30px">
+                        </a>
+                    </div>&nbsp;
+                    @if (Auth::check())
+                    @if (Auth::user()->id_role == 1 || Auth::user()->id_role == 2)
+                    <div class="dropdown" style="float: right;">
+                        <button class="dropbtn"><i class="fa fa-user-circle"></i>&nbsp;{{ Auth::user()->full_name }} </button>
+                        <div class="dropdown-content">
+                            <a href="{{ route('admin') }}">{{ __('Information') }}</a>
+                            <a href="{{ url('logout') }}">{{ __('logout') }}</a>
+                        </div>
+                    </div>
+                    @else
+                    <div class="dropdown"style="float: right;">
+                        <button class="dropbtn" ><i class="fa fa-user-circle"></i>&nbsp;{{ Auth::user()->full_name }}</button>
+                        <div class="dropdown-content">
+                            <a href="{{ route('info',Auth::user()->id) }}">{{ __('Information') }}</a>
+                            <a href="{{ url('logout') }}">{{ __('logout') }}</a>
+                        </div>
+                    </div>
+                    @endif
+                    @else
+                    <div class="dropdown" style="float: right">
+                        <a href="{{ route('login') }}"><button class="dropbtn">{{ __('login') }}</button></a>
+                    </div>
+                    @endif
+                </div>
+
+            </div>
+
+        </div>
+
+
+
+    </div>
+    <div class="main-menu">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand text-center" href="{{ route('index') }}"><img style=" height: 140px ;width:50%" src="images/logobook.png" alt="logo"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+            </nav>
+        </div>
+    </div>
+    <div class="main-menu">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div id="menu">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="menu_item down"><a href="#"><i class="fa fa-bars"></i> {{ __('catelory') }}</a>
+                                <div class="sub_menu">
+                                    <div class="sub_menu_block" style="width:75px">
+                                        <ul>
+                                            @for($i = 0; $i < count($product_n); $i++) <li><a href="{{ route('product_type', $types_id[$i]) }}">{{ $types_name[$i] }} ({{ $product_n[$i] }})</a>
+                            </li>
+                            @endfor
+                        </ul>
+                    </div>
+                </div>
+                </li>
+                <li class="menu_item down"><a href="{{ route('index') }}">{{ __('hompage') }}</a> </li>
+                <li class="menu_item down"><a href="{{ route('introduce') }}">{{ __('introduce') }} </a></li>
+                <li class="menu_item down"><a href="{{ route('news') }}">{{ __('newws') }}</a></li>
+                <li class="menu_item down"><a href="{{ route('all_book') }}">{{ __('all') }}</a>
+                    <div class="sub_menu">
+                        <div class="sub_menu_block" style="width:25px">
+                            <ul>
+                                <li><a href="{{ route('allnew') }}">{{ __("newbook") }}</a>
+                                </li>
+                                <li><a href="{{ route('allsale') }}">{{ __("salebook") }}</a>
+                                </li>
+                                <li><a href="{{  route('allhighlights')  }}">{{ __("hotbook") }}</a>
+                                </li>
+                            </ul>
+
+                        </div>
+                    </div>
+                </li>
+                <li class="menu_item down"><a href="#"><i class="fa fa-bars"></i> {{ __('company') }}</a>
+                    <div class="sub_menu">
+                        <div class="sub_menu_block" style="width:50px">
+                            <ul>
+                                @foreach ($company as $com)
+                                <li><a href="{{ route('product_company', $com->id) }}">{{ $com->name }}</a>
+                                </li>
+                                @endforeach
+                            </ul>
+
+                        </div>
+                    </div>
+                </li>
+                </ul>
+
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" style="margin-left: 5%">
+            <div class="cart my-2 my-lg-0">
+                <a href="{{ route('cart') }}">
+                    <span><i class="fa fa-shopping-cart" aria-hidden="true"></i></span></a>
+                <span class="quntity">
+                    @if (Session::has('cart'))
+                    {{ Session('cart')->totalQty }}@else 0
+                    @endif
+                </span>
+            </div>
+            <form class="form-inline my-2 my-lg-0" role="search" method="get" id="searchform" action="{{ route('search') }}">
+                <input class="form-control mr-sm-2" type="text" value="" name="key" id="s" placeholder="Nhập từ khóa..." autocomplete="off" />
+                <span class="fa fa-search"></span>
+            </form>
+        </div>
+        </nav>
+    </div>
+    </div>
+</header>
